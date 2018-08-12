c540acf313876cf21e36197c71b9425573b9ae59
Jay Sisodiya <jaysisodiya@gmail.com>
2018-08-12 10:06:49 -0700

    Update cr_kafka_topics_bycountry.py

--- ingestion/testdir/cr_kafka_topics_bycountry.py
+++ ingestion/testdir/cr_kafka_topics_bycountry.py
@@ -20,7 +20,7 @@ def myser(msg):
 #
 # Initialize the producer
 #
-brokerlist='ec2-54-186-208-110.us-west-2.compute.amazonaws.com:9092,ec2-52-11-172-126.us-west-2.compute.amazonaws.com:9092,ec2-52-88-204-111.us-west-2.compute.amazonaws.com:9092,ec2-52-35-101-204.us-west-2.compute.amazonaws.com:9092'
+brokerlist=''
 producer = KafkaProducer(bootstrap_servers=brokerlist, key_serializer=myser, value_serializer=myser)
 
 #
